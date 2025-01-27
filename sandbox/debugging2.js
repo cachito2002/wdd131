83e18d189589560d2f56d9ec9ac1df3969c5a09e
cachito2002 <fer21012@byui.edu>
2025-01-27 16:11:21 -0700

    ye

---
+++ sandbox/debugging2.js
@@ -0,0 +1,16 @@
+const PI = 3.14;
+let area = 0;
+function circleArea(radius) {
+    const area = radius * radius * PI;
+    return area
+}
+
+
+area = circleArea(3)
+console.log("Area1", area);
+
+area = circleArea(4)
+console.log("Area2", area)
+
+area = circleArea(1)
+console.log("Area3", area)
