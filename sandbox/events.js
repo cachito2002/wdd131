24b7b85aa4a4ef34c3fe5f3e024e07d1bda6419c
cachito2002 <fer21012@byui.edu>
2025-01-28 11:01:33 -0700

    tried to finish, and couldnt

--- sandbox/events.js
+++ sandbox/events.js
@@ -1,11 +1,53 @@
 let tasks = [];
 
 function renderTasks(tasks) {
+    const list = document.getElementById('todoList');
+    list.innerHTML = '';
+
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        if (task.completed) {
+            li.classList.add('strike');
+        }
+            
+
+        li.innerHTML = `
+            <li ${task.completed ? 'class="strike"' : ""}>
+            <p>${task.detail}</p>
+            <div>
+                <span data-action="delete">❎</span>
+                <span data-action="complete">✅</span>
+            </div>
+            </li>
+            `;
+
+            const deleteButton = li.querySelector('[data-action="delete"]');
+            const completeButton = li.querySelector('[data-action="complete"]')
+
+            deleteButton.addEventListener('click', () => removeTask(li));
+            completeButton.addEventListener('click', () => completeTask(li));
+        list.appendChild(li);
+    });
   // get the list element from the DOM
   // loop through the tasks array. transform (map) each task object into the appropriate HTML to represent a to-do.
 }
 
 function newTask() {
+    const input = document.getElementById('todo')
+    const task = input.value;
+    
+
+    if (task) {
+        const newTask = {
+            detail: task, 
+            completed: false
+        };
+
+    tasks.push(newTask);
+    renderTasks(tasks)
+    }
+
+    input.value = '';
   // get the value entered into the #todo input
   // add it to our arrays tasks
   // render out the list
@@ -47,5 +89,8 @@ function manageTasks(event) {
   // use that in a couple of if statements to decide whether to run removeTask or completeTask
 }
 
+document.getElementById('submitTask').addEventListener('click', newTask);
+
+document.getElementById
 // Add your event listeners here
 // We need to attach listeners to the submit button and the list. Listen for a click, call the 'newTask' function on submit and call the 'manageTasks' function if either of the icons are clicked in the list of tasks.
