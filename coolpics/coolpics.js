c2ac1991674f067e979d157e05976c9345127722
cachito2002 <fer21012@byui.edu>
2025-01-31 22:37:53 -0700

    I had to comment out the code form the HTML!!!

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,24 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menubutton = document.querySelector("#menubutton");
+    if (menubutton) {
+        menubutton.addEventListener("click", function() {
+            menu.classList.toggle("navhide");
+        });
+    }
     const menu = document.querySelector("#menu");
 
     menubutton.addEventListener("click", function() {
         menu.classList.toggle("navhide");
     });
-});
+    document.querySelectorAll(".gallery img").forEach(img => {
+        img.addEventListener("click", viewHandler);
+    });    
 
-function handleResize() {
-    const menu = document.querySelector("#menu");
-    if (window.innerWidth > 1000){
-        menu.classList.remove("navhide");
-    } else {
-        menu.classList.add("navhide");
-        }
-    }    
-    handleResize();
-    window.addEventListener("resize", handleResize);
+   
+    function handleResize() {
+        const menu = document.querySelector("#menu");
+        if (window.innerWidth > 1000){
+            menu.classList.remove("navhide");
+        } else if (!menu.classList.contains("navhide")) {
+            menu.classList.add("navhide");
+            }
+        }  
+        handleResize();
+        window.addEventListener("resize", handleResize); 
+    
 
     function viewHandler(event) {
+        const clickedImage = event.target;
+        const imagePath = clickedImage.src;
+        const splitPath = imagePath.split("-");
+        const fullImagePath = splitPath[0] + "-full.jpeg";
+        const altText = clickedImage.alt;
+
+        document.body.insertAdjacentHTML(
+            "afterbegin",
+            viewerTemplate(fullImagePath, altText)
+        );
+        const closeButton = document.querySelector(".close-viewer");
+        closeButton.addEventListener("click", closeViewer);
+    }
         // create a variable to hold the element that was clicked on from event.target
     
         // get the src attribute from that element and 'split' it on the "-"
@@ -29,7 +51,9 @@ function handleResize() {
         // (element.insertAdjacentHTML("afterbegin", htmltoinsert))
     
         // add a listener to the close button (X) that calls a function called closeViewer when clicked
-    
+    function closeViewer() {
+        const viewer = document.querySelector(".viewer");
+        viewer.remove();
     }
     function viewerTemplate(pic, alt) {
         return `<div class="viewer">
@@ -38,3 +62,4 @@ function handleResize() {
         </div>`;
     }
     
+});
