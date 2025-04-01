8aed81f70ea1a8d0325219df08f6d821b3b7557d
cachito2002 <fer21012@byui.edu>
2025-03-31 19:35:40 -0600

    finished the front home page

---
+++ myprofile/info.js
@@ -0,0 +1,44 @@
+const info = [
+    {
+        // each image will have an explanation. This one is for the family
+        image: './images/preparation.jpg',
+        title: 'BECOME THE COOK',
+        description: [
+            'You will learn how to prepare homemade mexican recipes from other experienced cook; soon to be you.',
+            'Easy to make with simple ingredients.',
+            'Delicious, feels like home; Mexico!',
+        ],   
+    },
+    {
+        image: './images/mexico.jpg',
+        title: 'DISCOVER MEXICO',
+        description: [
+            'Learn how to cook MEXICAN STYLE',
+            'Many have wanted to do it for a while now, learn the ingredients and learn how to do them',
+            'Mexico runs in my blood and its my passion to represent',
+            'The people are amazing, love is shown to their friends and family through their cooking',
+            'You can do that as well now',
+        ],
+    },
+    {
+        image: 'images/family.jpg',
+        title: 'LEARN IT FOR THE FAM!!!',
+        description: [
+            'Cooking is meant to be fun',
+            'Discover recipes / make them for the family',
+            'Overall, this site was made for you, to discover and add your own style',
+            'Feel free to change it up and make it your own',
+            'Enjoy the site and have FUN! This website was made more you!',
+        ]
+    }
+
+
+
+
+
+
+
+
+
+
+]
